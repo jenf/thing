887ff53573504688093ee10d6dadcde7898cb28a
Jennifer Freeman <jen.a.freeman@googlemail.com>
2012-03-20 21:54:14 +0000

    Add ability for things to vote differently to humans, cannibalism and bloodlust parameters

--- thing.py
+++ thing.py
@@ -15,13 +15,17 @@ class GameConfig(object):
     # players is total number of players (including things)
     # start_things is number of things at start of game
     # tie_break is True if ties result in a test
-    # bloodlust is probability of humans voting for a test
+    # bloodlust_human is probability of humans voting for a test - 1 for always
+    # bloodlust_thing is probability of things voting for a test for a human - 1 for always
+    # cannibalism is probability of things voting for a thing to be tested - 1 for always
     # retries is array of number of retries for each round (or 1 if not included)
-    def __init__(self, players, start_things, tie_break, bloodlust, retries):
+    def __init__(self, players, start_things, tie_break, bloodlust_human, bloodlust_thing, cannibalism, retries):
         self.players = players
         self.start_things = start_things
         self.tie_break = 1 if tie_break else 0
-        self.bloodlust = bloodlust
+        self.bloodlust_human = bloodlust_human
+        self.bloodlust_thing = bloodlust_thing
+        self.cannibalism = cannibalism
         self.retries = retries
     def retry_count(self, n):
         if n >= len(self.retries):
@@ -29,7 +33,7 @@ class GameConfig(object):
         return self.retries[n]
 
     def __str__(self):
-        return "Players: %i Tie_break: %i Start Things: %i Bloodlust: %f Retries: %s" % (self.players, self.tie_break, self.start_things, self.bloodlust, str(self.retries))
+        return "Players: %i Tie_break: %i Start Things: %i Human Bloodlust: %f ThingBloodlust: %f Thing Cannibalism: %f Retries: %s" % (self.players, self.tie_break, self.start_things, self.bloodlust_human, self.bloodlust_thing, self.cannibalism, str(self.retries))
 
 class Histogram(object):
     def __init__(self):
@@ -65,8 +69,22 @@ class Player(object):
         self.is_knowing_thing = False
 
     def vote(self, player):
-# Always vote
-        return True
+        prob = 1
+        if self.is_human():
+            prob = config.bloodlust_human
+        elif self.is_knowing_thing:
+            # New thing votes the same as a human
+            prob = config.bloodlust_human
+        elif player.is_thing():
+            prob = config.cannibalism
+        else:
+            prob = config.bloodlust_thing
+        if random.random() < prob:
+            return True
+        return False
+
+# Always vote yes
+#        return True
 
     def __repr__(self):
         return str(self.type)
@@ -175,7 +193,6 @@ def play_game(config):
            votes = config.retry_count(rounds)
            debug("At retry "+str(votes))
        if (state.run_vote()==False):
-#           print "Votes %i" % votes
            votes = votes-1
        else:
 # Check win conditions
@@ -183,116 +200,6 @@ def play_game(config):
            if (win):
               debug("Won "+str(whowon)+" "+str(rounds))
               return (whowon, rounds, state.number_things())
-
-        
-
-
-    return
-
-
-
-    def random_votes(n):
-        res = 0
-        while n > 0:
-            if random.random() < config.bloodlust:
-                res += 1
-            n -= 1
-        return res
-
-    def is_human(n):
-        return n >= nt
-    def knows_things(n):
-        return n < nk
-
-    rounds = 1
-    ntot = config.players
-    nk = 1
-    nt = nk
-    if start_night:
-        nt += 1
-    nh = ntot - nt
-    new_round = False
-    tested = 0
-        
-    retry_count = config.retry_count(0)
-
-    while True:
-        if (nk * 2) >= ntot:
-            debug("Things win")
-            return (True, rounds, ntot)
-        if nt == 0:
-            debug("Humans win")
-            return (False, rounds, ntot)
-        if ntot == 2:
-            debug("Tie")
-            return (True, rounds, ntot)
-        if new_round:
-            rounds += 1
-            new_round = False
-            if tested > 0:
-                tested = 0
-                #tested -= 1
-        debug("%d things%s, %d humans(%d tested).  Discuss...",
-            (nt, " (1 new)" if nt != nk else "", nh, tested))
-        # Pick someone at random to make a nomination
-        nom = random.randint(0, ntot - 1)
-        if knows_things(nom):
-            if nh == tested:
-                # All humans have been tested
-                test = random.randint(0, nt - 1)
-            else:
-                # Pick a human
-                test = nt
-        else:
-            # Pick at random
-            test = random.randint(0, ntot - (2 + tested))
-            if test >= nom:
-                test += 1
-
-        debug("%d nominated %d", (nom, test))
-        if is_human(test):
-            # Knowing things vote yes
-            # Everyone else votes randomly
-            yes = nk + random_votes(ntot - (nk + 1))
-        elif knows_things(test):
-            # Knowing things vote no
-            # Everyone else votes randomly
-            yes = random_votes(ntot - nk)
-        else: # New thing noninated
-            # Knowing things vote no
-            # Humans vote randomly
-            yes = random_votes(nh)
-        debug("%d votes", yes)
-        if (yes * 2) + config.tie_break <= ntot:
-            debug("Vote failed")
-            # Vote failed.  Go again.
-            continue
-        # Vote passed
-        if is_human(test):
-            debug("Tested Human")
-            tested += 1
-            if retry_count > 0:
-                debug("Retry")
-                retry_count -= 1;
-            else:
-                debug("End of Round %d", (rounds))
-                # End of round
-                nk = nt
-                nt = nk + 1
-                nh -= 1
-                retry_count = config.retry_count(rounds)
-                new_round = True
-        elif knows_things(test):
-            debug("Flushed Thing")
-            nk -= 1
-            nt -= 1
-            ntot -= 1
-            retry_count = 0
-        else: # New thing
-            debug("Flushed new Thing")
-            nt -= 1
-            ntot -= 1
-            retry_count = 0
 
 def montecarlo(config, niter):
     human = 0
@@ -309,9 +216,9 @@ def montecarlo(config, niter):
             hlen.add(rounds)
     return (human / float(human + thing), hlen, tlen)
 
-config = GameConfig(10, 2, True, 0.7, [2]*3)
+config = GameConfig(10, 2, True, 0.7, 1, 0, [2]*5)
 if do_debug==False:
- (percent, hlen, tlen) = montecarlo(config, 5000)
+ (percent, hlen, tlen) = montecarlo(config, 500)#00)
 else:
  (percent, hlen, tlen) = montecarlo(config, 1)
 
